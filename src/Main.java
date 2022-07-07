a02e561257004a3bcbd8bc1922660afce33ea8b9
surya <suryakanthr21@gmail.com>
2022-07-07 21:23:16 +0530

    Ticket availability feature added

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 import java.util.*;
 
+import javax.lang.model.util.ElementScanner14;
+
 //
 
 public class Main {
 
     static Map<Integer, Passenger> userDetails = new HashMap<>();
-    public static void main(String[] args){
+    static Map<Integer, Ticket> ticketDetails = new HashMap<>();
+
+    public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            
+
             boolean loop = true;
-            
-            while(loop){
+
+            while (loop) {
                 System.out.println("---------------------------------------------");
                 System.out.println(" Welcome to Flight Ticket reservation system");
                 System.out.println("---------------------------------------------");
@@ -22,11 +26,13 @@ public class Main {
                 System.out.println("0) Exit");
                 System.out.println("Enter option to proceed further");
 
-                int option = Integer.parseInt(sc.nextLine());
+                int option = sc.nextInt();
+                //int option = Integer.parseInt(sc.nextLine());
+                sc.nextLine();
 
-                switch(option){
-                    case 1:{
-                        
+                switch (option) {
+                    case 1: {
+
                         System.out.println("Enter your name");
                         String name = sc.nextLine();
 
@@ -46,29 +52,31 @@ public class Main {
                         String state = sc.nextLine();
 
                         Passenger p = new Passenger(name, emailId, contactNumber, street, city, state);
-                        if(userDetails.containsKey(p.id)){
+                        if (userDetails.containsKey(p.id)) {
                             System.out.println("User is already registered");
+                        } else {
+                            userDetails.put(p.id, p);
+                            System.out.println("User registered successfully with ID: " + p.id);
                         }
-                        else{
-                            userDetails.put(p.id,p);
-                            System.out.println("User registered successfully with ID: "+p.id);
-                        }
-                        
-                    } 
-                    break;
+
+                    }
+                        break;
 
                     case 2:
                         System.out.println("Please enter your registration ID");
-                        int checkID = Integer.parseInt(sc.nextLine());
-                        if(!userDetails.containsKey(checkID))
+                        int checkID = sc.nextInt();
+                        //int checkID = Integer.parseInt(sc.nextLine());
+                        if (!userDetails.containsKey(checkID))
                             System.out.println("Kindly register your details to book tickets");
-                        else{
+                        else {
                             System.out.println("1) Book regular ticket");
                             System.out.println("2) Book tourist ticket");
-                            int ticketType = Integer.parseInt(sc.nextLine());
-                            if(ticketType==1){
+                            sc.nextLine();
+                            int ticketType = sc.nextInt();
+                            if (ticketType == 1) {
                                 System.out.println("Enter the following details");
                                 System.out.print("Departure location");
+                                sc.nextLine();
                                 String departureLocation = sc.nextLine();
                                 System.out.print("Destination location");
                                 String destinationLocation = sc.nextLine();
@@ -77,13 +85,22 @@ public class Main {
                                 System.out.print("Enter number of tickets");
                                 int noOfTickets = sc.nextInt();
 
-                                RegularTicket regTkt = new RegularTicket(departureLocation, destinationLocation, departureDate, noOfTickets, userDetails.get(checkID));
+                                RegularTicket regTkt = new RegularTicket(departureLocation, destinationLocation,
+                                        departureDate, noOfTickets, userDetails.get(checkID));
                                 System.out.println(noOfTickets + "regular tickets booked successfully");
+                                ticketDetails.put(checkID,regTkt);
                             }
                         }
 
-                    break;
+                        break;
                     case 3:
+                        System.out.println("Please enter your registration ID");
+                        int ticketCheckID = sc.nextInt();
+                        if(!ticketDetails.containsKey(ticketCheckID))
+                            System.out.println("No tickets booked from this userID");
+                        else{
+                            System.out.println(ticketDetails.get(ticketCheckID).flight.checkSeatAvailability());
+                        }
                         break;
                     case 4:
                         break;
@@ -99,6 +116,6 @@ public class Main {
                 }
             }
         }
-            
+
     }
 }
